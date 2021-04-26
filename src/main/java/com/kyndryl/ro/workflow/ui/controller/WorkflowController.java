e6368b5acbbe50877b612a3131d4da3030e5b1cc
Abdul426 <munnaf426@gmail.com>
2021-04-26 10:58:28 +0530

    Added memory SOP

--- src/main/java/com/kyndryl/ro/workflow/ui/controller/WorkflowController.java
+++ src/main/java/com/kyndryl/ro/workflow/ui/controller/WorkflowController.java
@@ -3,7 +3,12 @@
  */
 package com.kyndryl.ro.workflow.ui.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +20,44 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "workflow")
 public class WorkflowController {
+
+	@GetMapping(value = "/request/{reqId}")
+	public String getVersion(@PathVariable("reqId") String reqId) {
+
+		Map<String, WorkflowController> map = new HashMap();
+		StringBuffer memoryDetails = new StringBuffer();
+		try {
+			System.out.println("Processing reqId: " + reqId);
+			Thread.sleep(1000 * 3);
+			for (int i = 1; i <= 100000; i++)
+				map.put(reqId + new Random().nextInt(), new WorkflowController());
+
+			Runtime runtime = Runtime.getRuntime();
+			// Run the garbage collector
+			// runtime.gc();
+			// Calculate the used memory
+
+			long memory = runtime.totalMemory() - runtime.freeMemory();
+			System.out.println("Max memory is Megabytes: " + bytesToMegabytes(runtime.maxMemory()));
+			memoryDetails.append(", Max Memory: " + bytesToMegabytes(runtime.maxMemory()));
+			System.out.println("Total memory is Megabytes: " + bytesToMegabytes(runtime.totalMemory()));
+			memoryDetails.append(", Total Memory: " + bytesToMegabytes(runtime.totalMemory()));
+			System.out.println("Used memory is bytes: " + memory);
+			memoryDetails.append(", Used Memory: " + bytesToMegabytes(memory));
+			System.out.println("Used memory is megabytes: " + bytesToMegabytes(memory));
+
+			System.out.println("Processing reqId: " + reqId + " Added few objects. Size: " + map.size());
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return "V1. HostName: " + System.getenv("HOSTNAME") + " Memeory Details: " + memoryDetails.toString();
+	}
+
+	private static final long MEGABYTE = 1024L * 1024L;
+
+	public static long bytesToMegabytes(long bytes) {
+		return bytes / MEGABYTE;
+	}
 
 	@GetMapping(value = "/version")
 	public String getVersion() {
