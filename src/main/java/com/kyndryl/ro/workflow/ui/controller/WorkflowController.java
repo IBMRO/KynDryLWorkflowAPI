acff014c5131fecefa294c874a3145ed3bed55f6
Abdul426 <munnaf426@gmail.com>
2021-04-26 16:49:22 +0530

    Update WorkflowController.java

--- src/main/java/com/kyndryl/ro/workflow/ui/controller/WorkflowController.java
+++ src/main/java/com/kyndryl/ro/workflow/ui/controller/WorkflowController.java
@@ -26,18 +26,18 @@ public class WorkflowController {
 	@GetMapping(value = "/request/{reqId}")
 	public String getVersion(@PathVariable("reqId") String reqId) {
 
-		Map<String, HashMap<String, WorkflowController>> map = new HashMap();
-		List<HashMap> myL = new ArrayList<HashMap>();
+		Map<String, WorkflowController> map = new HashMap();
+		//List<HashMap> myL = new ArrayList<HashMap>();
 		StringBuffer memoryDetails = new StringBuffer();
 		try {
 			System.out.println("Processing reqId: " + reqId);
 
 			HashMap h2 = null;
-			for (int i = 1; i <= 500; i++) {
-				h2 = new HashMap<String, WorkflowController>();
-				h2.put(reqId + new Random().nextInt(), new WorkflowController());
-				myL.add(h2);
-				map.put(reqId + new Random().nextInt(), h2);
+			for (int i = 1; i <= 100000; i++) {
+				//h2 = new HashMap<String, WorkflowController>();
+				//h2.put(reqId + new Random().nextInt(), new WorkflowController());
+				//myL.add(h2);
+				map.put(reqId + new Random().nextInt(), new WorkflowController());
 			}
 
 			Runtime runtime = Runtime.getRuntime();
